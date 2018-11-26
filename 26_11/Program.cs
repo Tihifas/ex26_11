f36522b4f430b7ea92382dda78962bf5d57aa540
Torstein Bratberg (Kursus Lokale) <tdbratberg@gmail.com>
2018-11-26 10:29:43 +0100

    Add project files.

---
+++ 26_11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _26_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
